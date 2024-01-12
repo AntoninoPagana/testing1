34ce12a95062d86ae7bb18cd3f296d0ed8968f77
Antonino <pagana899@gmail.com>
2024-01-12 15:02:32 +0100

    esercizio completo pt2

--- Main.java
+++ Main.java
@@ -6,20 +6,8 @@ public class Main {
 
     public static Integer somma(Integer a, Integer b) {
         if (a == null || b == null) {
-            return null;
+            throw new ArithmeticException();
         }
         return a + b;
-    }
-
-    public static Integer sottrazione(Integer a, Integer b) {
-        return a - b;
-    }
-
-    public static Double divisione(Double a, Double b) {
-        return a / b;
-    }
-
-    public static Integer moltiplicazione(Integer a, Integer b) {
-        return a * b;
     }
 }
--- MainTest.java
+++ MainTest.java
@@ -1,26 +1,31 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-    private final Main testing = new Main();
 
     @Test
     void somma_di_due_interi() {
-        Integer result = testing.somma(5, 10);
+        Integer result = Main.somma(5, 10);
         assertEquals(15, result, "La somma di 5 e 10 dovrebbe risultare 15");
     }
 
     @Test
-    void somma_se_presente_null() {
-        Integer result = testing.somma(null, 10);
-        assertNull(result);
-    }
-
-    @Test
     void somma_di_due_non_interi() {
-        Integer result = testing.somma(-10, -25);
+        Integer result = Main.somma(-10, -25);
         assertEquals(-35, result, "La somma di -10 e -25 dovrebbe risultare -35");
     }
+
+    //Cercato su Internet (Come gestire un eccezione aritmetica in testing)
+    @Test
+    public void somma_gestioneEccezione() {
+        assertThrows(ArithmeticException.class, () -> {
+            Main.somma(null, 3);
+        });
+
+        assertThrows(ArithmeticException.class, () -> {
+            Main.somma(2, null);
+        });
+    }
+
 }
