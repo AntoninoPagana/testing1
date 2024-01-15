57c4cfde687e40ae3e9620d5ddf91e38177d0bd7
Antonino <pagana899@gmail.com>
2024-01-15 23:35:44 +0100

    esercizio completo pt4

--- MainTest.java
+++ MainTest.java
@@ -3,29 +3,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    Main testing = new Main();
 
     @Test
     void sommaDiDueInteri() {
-        Integer result = Main.somma(5, 10);
+        Integer result = testing.somma(5, 10);
         assertEquals(15, result, "La somma di 5 e 10 dovrebbe risultare 15");
     }
 
     @Test
     void sommaDiDueNonInteri() {
-        Integer result = Main.somma(-10, -25);
+        Integer result = testing.somma(-10, -25);
         assertEquals(-35, result, "La somma di -10 e -25 dovrebbe risultare -35");
     }
 
-    //Cercato su Internet (Come gestire un eccezione aritmetica in testing)
     @Test
     public void sommaGestioneEccezioneNull() {
-        assertThrows(ArithmeticException.class, () -> {
-            Main.somma(null, 3);
-        });
+        try {
+            Integer result = testing.somma(null, 5);
+            assertNull(result);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
 
-        assertThrows(ArithmeticException.class, () -> {
-            Main.somma(2, null);
-        });
     }
 
 }
